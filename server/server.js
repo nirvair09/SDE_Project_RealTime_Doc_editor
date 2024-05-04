6fcd49c3abd23c4990f72e7248551d6411e779cf
Chandradeep Roy <140598046+nirvair09@users.noreply.github.com>
2024-05-04 14:14:06 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,63 +1,3 @@
-// const mongoose = require("mongoose")
-// const Document = require("./Document")
-// const dotenv = require("dotenv");
-
-// dotenv.config();
-
-// mongoose.connect(process.env.MONGO_URL, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useFindAndModify: false,
-//   useCreateIndex: true,
-// }).then(() => {
-//   console.log("MongoDB connected");
-// }).catch(err => {
-//   console.error("MongoDB connection error:", err);
-// });
-
-// const io = require("socket.io")(3001, {
-//   cors: {
-//     origin: "*",
-//     methods: ["GET", "POST"],
-//   },
-// })
-
-// console.log("Socket server listening");
-
-// const defaultValue = ""
-
-// io.on("connection", socket => {
-//   console.log("Client connected");
-
-//   socket.on("get-document", async documentId => {
-//     console.log("Fetching document:", documentId);
-//     const document = await findOrCreateDocument(documentId)
-//     socket.join(documentId)
-//     socket.emit("load-document", document.data)
-
-//     socket.on("send-changes", delta => {
-//       socket.broadcast.to(documentId).emit("receive-changes", delta)
-//     })
-
-//     socket.on("save-document", async data => {
-//       await Document.findByIdAndUpdate(documentId, { data })
-//     })
-//   })
-// })
-
-// async function findOrCreateDocument(id) {
-//   if (id == null) return
-
-//   const document = await Document.findById(id)
-//   if (document) {
-//     console.log("Document found:", id);
-//     return document;
-//   }
-//   console.log("Creating new document:", id);
-//   return await Document.create({ _id: id, data: defaultValue })
-// }
-
-
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
@@ -123,5 +63,5 @@ async function findOrCreateDocument(id) {
 }
 
 server.listen(PORT, () => {
-  console.log(Server is running on port ${PORT});
+  console.log(`Server is running on port ${PORT}`);
 });
