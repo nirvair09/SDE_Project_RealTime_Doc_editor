2ec322bcdc7e9508500e979e677ce59fa4cf702a
Chandradeep Roy <140598046+nirvair09@users.noreply.github.com>
2024-05-05 08:43:03 +0530

    Update TextEditor.js

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -23,7 +23,7 @@ export default function TextEditor() {
   const [quill, setQuill] = useState()
 
   useEffect(() => {
-    const s = io("https://sde-project-realtime-doc-editor.onrender.com")
+    const s = io("https://sde-project-realtime-backend.onrender.com/")
     setSocket(s)
 
     return () => {
