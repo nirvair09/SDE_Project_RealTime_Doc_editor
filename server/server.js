9b3d357f3e714423e26f1e167d9842eb49b0970b
Chandradeep Roy <140598046+nirvair09@users.noreply.github.com>
2024-05-05 08:48:00 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,72 @@
+// const express = require("express");
+// const http = require("http");
+// const mongoose = require("mongoose");
+// const Document = require("./Document");
+// const dotenv = require("dotenv");
+// const socketio = require("socket.io");
+
+// dotenv.config();
+
+// const app = express();
+// const server = http.createServer(app);
+// const io = socketio(server);
+
+// const defaultValue = "";
+
+// // Express middleware for JSON parsing
+// app.use(express.json());
+
+// // MongoDB connection
+// mongoose.connect(process.env.MONGO_URL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: false,
+//   useCreateIndex: true,
+// });
+
+// // Socket.io connection
+// io.on("connection", (socket) => {
+//   socket.on("get-document", async (documentId) => {
+//     const document = await findOrCreateDocument(documentId);
+//     socket.join(documentId);
+//     socket.emit("load-document", document.data);
+
+//     socket.on("send-changes", (delta) => {
+//       socket.broadcast.to(documentId).emit("receive-changes", delta);
+//     });
+
+//     socket.on("save-document", async (data) => {
+//       await Document.findByIdAndUpdate(documentId, { data });
+//     });
+//   });
+// });
+
+// // Express route for handling API requests
+// app.get("/", (req, res) => {
+//   res.send("Server is running.");
+// });
+
+// // Helper function to find or create a document
+// async function findOrCreateDocument(id) {
+//   if (id == null) return;
+
+//   const document = await Document.findById(id);
+//   if (document) return document;
+//   return await Document.create({ _id: id, data: defaultValue });
+// }
+
+// // Start the server
+// const PORT = process.env.PORT || 3000;
+// server.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 const express = require("express");
 const http = require("http");
 const mongoose = require("mongoose");
 const Document = require("./Document");
 const dotenv = require("dotenv");
 const socketio = require("socket.io");
+const path = require("path");
 
 dotenv.config();
 
@@ -44,6 +107,14 @@ io.on("connection", (socket) => {
 // Express route for handling API requests
 app.get("/", (req, res) => {
   res.send("Server is running.");
+});
+
+// Serve static files from the 'build' directory
+app.use(express.static(path.join(__dirname, "build")));
+
+// Route all other requests to React's index.html file
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 // Helper function to find or create a document
